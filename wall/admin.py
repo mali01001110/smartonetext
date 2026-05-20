e2adb15e6d6cc3892181b97de3909e40eefec171
mali01001110 <yannzakpa31@gmail.com>
2026-05-20 09:25:03 +0000

    Update admin setup and message management

--- wall/admin.py
+++ wall/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "created_at")
+    search_fields = ("username", "message")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
